bc9b24b0a68ad9e04477a2778d37115b09b263f8
Rohit <rohit.singh11299@gmail.com>
2022-09-29 14:19:02 +0530

    functions added

--- index.js
+++ index.js
@@ -6,67 +6,107 @@ let europe = document.getElementsByClassName('europe')[0];
 let africa = document.getElementsByClassName('africa')[0];
 let americas = document.getElementsByClassName('americas')[0];
 let oceania = document.getElementsByClassName('oceania')[0];
-let country_data = document.getElementsByClassName('country-data')[0];
 const url = "https://restcountries.com/v2/all";
 const search_input = document.getElementById('search-input');
 let form = document.querySelector('form');
+let main_content = document.getElementsByClassName('main-content')[0];
 let container = document.getElementsByClassName('container')[0];
-let body = document.querySelector('body')
-// console.log(body.hasChildNodes());
 
-function info_of_particular_country(item){
+function info_of_particular_country(item) {
 
-    container.classList.toggle('display-remove');
+    main_content.classList.toggle('display-remove');
     let info_container = document.createElement('div');
-    let borders = [];
     info_container.classList.add = 'info-of-country';
+    let currencies = item.currencies[0].name;
+    // let borders = item.borders;
+    let languages_arr = [];
+    item.languages.forEach(ele => {
+        // console.log(ele.name)
+        languages_arr.push(ele.name)
+    })
+    let languages = ''
+    for(let item in languages_arr){
+        languages += ` ${languages_arr[item]},`
+    }
     info_container.innerHTML = `
     <div class="back-button">
     <i class="fa-solid fa-arrow-left-long"></i>
     <p>Back</p>
-</div>
-<div class="particular-country-content">
+    </div>
+    <div class="particular-country-content">
     <img src=${item.flag} alt="" class="particular-country-flag">
     <div class="country-info">
-        <h1 class="particular-country-name">${item.name}</h1>
-        <div class="particular-country-data">
-            <div class = 'left-data'>
-                <p>Native Name: <span class = "native-name">${item.nativename}</span></p>
+    <h1 class="particular-country-name">${item.name}</h1>
+    <div class="particular-country-data">
+    <div class = 'left-data'>
+    <p>Native Name: <span class = "native-name">${item.nativeName}</span></p>
                 <p>Population: <span class="particular-country-population">${item.population}</span></p>
                 <p>Region: <span class="particular-country-region">${item.region}</span></p>
                 <p>Sub Region: <span class="sub-region">${item.subregion}</span></p>
                 <p>Capital: <span class="particular-country-capital">${item.capital}</span></p>
-            </div>
+                </div>
             <div class="right-data">
-                <p>Top Level Domain: <span class="top-level-domain">${item.topleveldomain}</span></p>
-                <p>Currencies: <span class="currencies">${item.currencies}</span></p>
-                <p>Languages: <span class="languages">${item.languages}</span></p>
+            <p>Top Level Domain: <span class="top-level-domain">${item.topLevelDomain[0]}</span></p>
+            <p>Currencies: <span class="currencies">${currencies}</span></p>
+            <p>Languages: <span class="languages">${languages}</span></p>
             </div>
-        </div>
-        <div class="border-countries">
+            </div>
+            <div class="border-countries">
             <p class="border-countries-data">Border Countries: </p>
-            <div class="present-borders-countries">
-                <div class="boxes">bsbebbc</div>
-                <div class="boxes">bcbdiess</div>
+            <div class="present-borders-countries"></div>
             </div>
-        </div>
-    </div>
-</div>
-    `
+            </div>
+            </div>
+            `
+            let p_data = document.getElementsByClassName('p-data')[0];
+            p_data.appendChild(info_container);
+            let back_button = document.getElementsByClassName('back-button')[0];
+                    console.log(back_button)
+                    back_button.addEventListener('click',() =>{
+                        remove_show(p_data)
+                        p_data.classList.toggle('display-remove');
+                    })
+        }
+function remove_show(ele){
+    ele.classList.toggle('display-remove');
+    clear_child(ele)
+    main_content.classList.toggle('display-remove');
+}
+function border_countries(pre_bor_count, item){
+    for(let ele in item.borders) {
+        console.log(item.borders[ele]);
+        let child_element = document.createElement('div')
+        child_element.innerHTML = `<span class = 'box'>${item.borders[ele]}</span>`
+        console.log(child_element)
+        pre_bor_count.appendChild(child_element);
+    }
+}
+function append_to_list(ele) {
+    let div = document.createElement('div')
+    div.classList.add('country-data');
+    
+    div.innerHTML = `<img class = 'flag-img' src=${ele.flag} alt="">
+    <h3 class="country-flag">${ele.name}</h3>
+    <p >Population: <span class="population">${ele.population}</span></p>
+    <p >Region: <span class="region">${ele.region}</span></p>
+    <p >Capital: <span class="capital">${ele.capital}</span></p>`
+    
+    countries.appendChild(div);
 }
 function fetch_countries_data() {
     fetch(url)
-        .then(response => response.json())
-        .then(res => {
-            form.addEventListener('submit',(e) => {
-                e.preventDefault();
-                let is_Present = false;
+    .then(response => response.json())
+    .then(res => {
+        form.addEventListener('submit', (e) => {
+            e.preventDefault();
+            let is_Present = false;
                 const str = search_input.value
-                if(str !== ''){
+                if (str !== '') {
                     const val = str[0].toUpperCase() + str.substring(1);
                     clear_child(countries);
                     res.forEach(ele => {
-                        if(val == ele.name){
+                        console.log(ele)
+                        if (val == ele.name) {
                             append_to_list(ele);
                             search_input.value = ''
                             is_Present = true;
@@ -74,14 +114,14 @@ function fetch_countries_data() {
                         }
                     })
                     res.forEach(ele => {
-                        if(val !== ele.name && !is_Present){
+                        if (val !== ele.name && !is_Present) {
                             search_input.value = ''
                             append_to_list(ele);
                         }
                     })
                     return;
                 }
-                else if(str == ''){
+                else if (str == '') {
                     clear_child(countries);
                     res.forEach(ele => {
                         append_to_list(ele);
@@ -94,7 +134,7 @@ function fetch_countries_data() {
                 if (e.target.classList.contains('asia')) {
                     clear_child(countries);
                     res.forEach(ele => {
-                        if (ele.region == 'Asia'){
+                        if (ele.region == 'Asia') {
                             append_to_list(ele)
                         }
                     })
@@ -103,7 +143,7 @@ function fetch_countries_data() {
                 else if (e.target.classList.contains('africa')) {
                     clear_child(countries);
                     res.forEach(ele => {
-                        if (ele.region == 'Africa'){
+                        if (ele.region == 'Africa') {
                             append_to_list(ele)
                         }
                     })
@@ -112,7 +152,7 @@ function fetch_countries_data() {
                 else if (e.target.classList.contains('europe')) {
                     clear_child(countries);
                     res.forEach(ele => {
-                        if (ele.region == 'Europe'){
+                        if (ele.region == 'Europe') {
                             append_to_list(ele)
                         }
                     })
@@ -121,16 +161,16 @@ function fetch_countries_data() {
                 else if (e.target.classList.contains('americas')) {
                     clear_child(countries);
                     res.forEach(ele => {
-                        if (ele.region == 'Americas'){
+                        if (ele.region == 'Americas') {
                             append_to_list(ele)
                         }
                     })
                     return;
                 }
-                else if (e.target.classList.contains('oceania')){
+                else if (e.target.classList.contains('oceania')) {
                     clear_child(countries);
                     res.forEach(ele => {
-                        if (ele.region == 'Oceania'){
+                        if (ele.region == 'Oceania') {
                             append_to_list(ele)
                         }
                     })
@@ -148,28 +188,28 @@ function fetch_countries_data() {
             res.forEach(ele => {
                 append_to_list(ele)
             })
+
+            let country_data = document.getElementsByClassName('country-data');
+            for(let ele of country_data){
+                ele.addEventListener('click',(e) => {
+                    let content = ele.children[1].textContent;
+                    res.forEach(ele => {
+                        if(ele.name == content){
+                            info_of_particular_country(ele);
+                        }
+                    })
+                })
+            }
         })
         .catch(err => console.log(err))
 }
 
-function append_to_list(ele) {
-    let div = document.createElement('div')
-    div.classList.add('country-data');
-    
-    div.innerHTML = `<img class = 'flag-img' src=${ele.flag} alt="">
-    <h3 class="country-flag">${ele.name}</h3>
-    <p >Population: <span class="population">${ele.population}</span></p>
-    <p >Region: <span class="region">${ele.region}</span></p>
-    <p >Capital: <span class="capital">${ele.capital}</span></p>`
-    
-    countries.appendChild(div);
-    country_data.addEventListener('click',info_of_particular_country(ele))
-}
 function clear_child(class_name) {
     while (class_name.hasChildNodes()) {
         class_name.removeChild(class_name.lastChild);
     }
 }
+
 dropdown.addEventListener('click', () => {
     all_continent.classList.toggle('display-toggle')
 })
